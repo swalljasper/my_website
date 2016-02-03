5c29b61eda3a6f3a78422d08094e56ea5c40b593
SorenWalljasper <swalljasper@wisc.edu>
2016-02-02 20:42:21 -0600

    create interactivity
    Module 2 activity

--- js/main.js
+++ js/main.js
@@ -1,57 +1,108 @@
-// var mydiv = document.getElementById("mydiv");
-// mydiv.innerHTML = "Hello";
 
-function initialize(){
+
+function initialize(){ //calls the functions when the page initializes
 	cities();
+	addColumns(cityPop);
+	addEvents();
 };
 
-function cities(){
-	var cities = [
-	"Tokyo",
-	"New York",
-	"Sao Paulo",
-	"Seoul"
+var cityPop;
+
+function cities(){ //function that creates cities
+
+	cityPop = [ //array that contains cities data (2 demensional)
+		{
+			city: "Tokyo",
+			population: 33200000
+		},
+		{
+			city: "New York",
+			population: 17800000
+		},
+		{
+			city: "Sao Paulo",
+			population: 17700000
+		},
+		{
+			city: "Seoul",
+			population: 17500000
+		}
 	];
 
-	var population = [
-	33200000,
-	17800000,
-	17700000,
-	17500000
-	];
+	$("#mydiv").append("<table>"); //creates table within div (created in index.html)
 
-	var table = document.createElement("table");
+	$("table").append("<tr>"); //creates table rows within table
+
+	$("tr").append("<th>City</th><th>Population</th>"); //creates table header in tr
+
+	for (var i = 0;i < cityPop.length; i++){ //for loop inputs the array cityPop into the tds
+		var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+		$("table").append(rowHtml);
+	};
+};
+
+
+function addColumns(cityPop){ //function that adds columns 
+	console.log("ran addColumns");
+    
+    $('tr').each(function(i){ //establihes were appending the trs, with the jquery each method: runing through each one until its through all
+
+    	if (i == 0){ //basic if else statement
+
+    		$(this).append('<th>City Size</th>'); //if its the first row make it say City Size
+    	} else {
+
+    		var citySize; //declares city size variable locally
+    		
+    		if (cityPop[i-1].population < 17750000){ //if its not the first row and its under X assign value
+    			citySize = 'Large';
+
+    		} else if (cityPop[i-1].population < 20000000){ //if its not the first row and its under X assign value
+    			citySize = 'Very Large';
+
+    		} else {
+    			citySize = 'Very Very Large'; //if its not the first row and its under X assign value
+    		};
+
+    		$(this).append('<td>' + citySize + '</td>'); //append the tds with the values that were assigned
+    	};
+    });
+};
+
+function addEvents(){ //establishes addEvents function
+
 	
-	var headerRow = document.createElement("tr");
-	
-	var cityHeader = document.createElement("th");
-	cityHeader.innerHTML = "City";
-	headerRow.appendChild(cityHeader);
 
-	var popHeader = document.createElement("th");
-	popHeader.innerHTML = "Population";
-	headerRow.appendChild(popHeader);
+	$('table').mouseover (function(i) { //uses jquery mouseover function on the table and establishes a function of i
+		
+		var color = "rgb("; //establishes color variable with setting up the rgb(x,x,x) format
 
-	table.appendChild(headerRow);
+		for (var i=0; i<3; i++){ //for loop with varible i that runs 3x
 
-	for(var i = 0; i < cities.length; i++){
-		var tr = document.createElement("tr");
+			var random = Math.round(Math.random() * 255); //establishes a variable that will generate a random between 0 and 255
 
-		var city = document.createElement("td");
-		city.innerHTML = cities[i];
-		tr.appendChild(city);
+			color += random; //adds veriables together 
 
-		var pop = document.createElement("td");
-		pop.innerHTML = population[i];
-		tr.appendChild(pop);
+			if (i<2){ //if its the first or second rgb value have a comma afetr it to stick with proper format
+				color += ",";
+			
+			} else {
+				color += ")"; //if its the last one, put a ) afetr it to stick with format
+			};
+		};
 
-		table.appendChild(tr);
+		$(this).css('color', color); //appends css with the random created rgb colors
+	});
+
+	function clickme(){ //establishes click function
+
+		alert('Hey, you clicked me!'); //alert message
 	};
 
-	var mydiv = document.getElementById("mydiv");
-	mydiv.appendChild(table);
-
-
+	$('table').on('click', clickme); //establishes that this will be done on the table through jquery on method
 };
 
-window.onload = initialize();
+$(document).ready(initialize); //initializes functions when page is loaded
+
+
+ 
